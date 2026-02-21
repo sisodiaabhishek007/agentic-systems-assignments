f625c6517dde050daee26a49f2b2d3652599f01f
sisodiaabhishek007 <abhi.sisodia38@gmail.com>
2026-02-21 22:42:00 +0530

    ighest of Last Two Scores

--- python-essentials-2/Highest of Last Two Scores.py
+++ python-essentials-2/Highest of Last Two Scores.py
@@ -1,1 +1,23 @@
+class StudentScores:
+    
+    def __init__(self, scores):
+        self.scores = scores
 
+    def highest_last_two(self):
+        try:
+            # Check if scores are less than 2
+            if len(self.scores) < 2:
+                raise ValueError
+            
+            # Using negative indexing to get last two scores
+            highest = max(self.scores[-2:])
+            print(f"Highest score among last two is: {highest}")
+
+        except:
+            print("Not enough scores to find highest value")
+
+
+# Example Input
+scores = [45, 67, 89, 72]
+student = StudentScores(scores)
+student.highest_last_two()
