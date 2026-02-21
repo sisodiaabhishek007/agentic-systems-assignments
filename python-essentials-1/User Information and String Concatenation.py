1d918dbd4bc49b08a532529e1913d2212b4d670b
sisodiaabhishek007 <abhi.sisodia38@gmail.com>
2026-02-21 11:21:11 +0530

    User Information and String Concatenation

--- python-essentials-1/User Information and String Concatenation.py
+++ python-essentials-1/User Information and String Concatenation.py
@@ -1,1 +1,14 @@
+first_name = input("Enter your first name: ")
+last_name = input("Enter your last name: ")
+age_input = input("Enter your age: ")
 
+if not age_input.lstrip('-').isdigit():
+    print("Invalid age input")
+else:
+    age = int(age_input)
+    
+    if age < 0:
+        print("Age cannot be negative")
+    else:
+        print("Full Name: " + first_name + " " + last_name)
+        print("You will be", age + 1, "next year")
