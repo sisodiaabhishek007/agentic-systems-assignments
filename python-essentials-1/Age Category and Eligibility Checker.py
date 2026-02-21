128eb2f09249a24fd822b7714e96e9587ad7b9ba
sisodiaabhishek007 <abhi.sisodia38@gmail.com>
2026-02-21 11:25:16 +0530

    Age Category and Eligibility Checker.py

--- python-essentials-1/Age Category and Eligibility Checker.py
+++ python-essentials-1/Age Category and Eligibility Checker.py
@@ -1,1 +1,26 @@
+name = input("Enter your name: ")
+age_input = input("Enter your age: ")
 
+if not age_input.lstrip('-').isdigit():
+    print("Invalid age input")
+else:
+    age = int(age_input)
+    
+    if age < 0:
+        print("Age cannot be negative")
+    else:
+        print("Hello " + name)
+        
+        if age < 13:
+            print("You are a Child")
+        elif 13 <= age <= 17:
+            print("You are a Teenager")
+        elif 18 <= age <= 59:
+            print("You are an Adult")
+        else:
+            print("You are a Senior Citizen")
+        
+        if age >= 18:
+            print("You are eligible to vote")
+        else:
+            print("You are not eligible to vote")
