a21c2279efbf9c4d94bb30b72bc7f7172527c819
sisodiaabhishek007 <abhi.sisodia38@gmail.com>
2026-02-28 19:48:09 +0530

    User Registration Validation

--- Pydantic for Data Validation/User Registration Validation.py
+++ Pydantic for Data Validation/User Registration Validation.py
@@ -1,1 +1,20 @@
+from pydantic import BaseModel, EmailStr, Field, ValidationError
 
+class UserRegister(BaseModel):
+    username: str = Field(..., min_length=5)
+    email: EmailStr
+    age: int = Field(..., ge=18)
+
+
+# Example usage
+try:
+    user = UserRegister(
+        username="abhi123",
+        email="abhi@example.com",
+        age=20
+    )
+    print("Valid user:", user)
+
+except ValidationError as e:
+    print("Validation Error:")
+    print(e)
