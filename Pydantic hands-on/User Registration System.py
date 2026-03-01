3bad2d7af2bd05c0de4c1010a35faece3be0933f
sisodiaabhishek007 <abhi.sisodia38@gmail.com>
2026-03-01 20:03:21 +0530

    User registration system for ecommerce platform

--- Pydantic hands-on/User Registration System.py
+++ Pydantic hands-on/User Registration System.py
@@ -1,1 +1,24 @@
-ser
+from pydantic import BaseModel, Field, EmailStr
+from typing import Optional
+
+class Address(BaseModel):
+    city: str = Field(min_length = 3)
+    pincode: str = Field(min_length = 6, max_length = 6)
+
+address_info = {'city': 'Delhi', 'pincode': '110091'}
+
+add = Address(**address_info)
+
+class User(BaseModel):
+    user_id: int
+    name: str
+    email: EmailStr
+    age: int = Field(ge=18)
+    address : Address
+    is_premium: bool = False 
+
+user_info = {'user_id': '7', 'name': 'Abhi', 'email': 'abhi@gmail.com', 'age': '26', 'address' : address_info}
+
+user = User(**user_info)
+
+print(user.age)
