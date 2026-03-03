d493d5b5754d7d30b2a853f16110ffe4f4cf9e64
sisodiaabhishek007 <abhi.sisodia38@gmail.com>
2026-03-03 21:10:23 +0530

    fastapi main.py

--- fastapi-fundamentals/main.py
+++ fastapi-fundamentals/main.py
@@ -1,1 +1,16 @@
+from fastapi import FastAPI, Query
+from pydantic import BaseModel
+from typing import Optional
 
+app = FastAPI()
+
+class SearchParams(BaseModel):
+    name: Optional[str] = None
+    age: Optional[int] = None
+
+@app.get("/search")
+def search(params: SearchParams = Query()):
+    return {
+        "name": params.name,
+        "age": params.age
+    }
