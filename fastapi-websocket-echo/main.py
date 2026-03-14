7ac8ee79bfc559c9f6b3c1f8d54a8ebc5adccd6f
sisodiaabhishek007 <abhi.sisodia38@gmail.com>
2026-03-14 21:08:00 +0530

    fastapi websockets assignement submission

--- fastapi-websocket-echo/main.py
+++ fastapi-websocket-echo/main.py
@@ -1,1 +1,21 @@
+from fastapi import FastAPI, WebSocket, WebSocketDisconnect
 
+app = FastAPI()
+
+
+@app.websocket("/ws")
+async def websocket_endpoint(websocket: WebSocket):
+    await websocket.accept()
+    print("Client connected")
+
+    try:
+        while True:
+            # Receive message from client
+            message = await websocket.receive_text()
+
+            # Send response back
+            response = f"Server received: {message}"
+            await websocket.send_text(response)
+
+    except WebSocketDisconnect:
+        print("Client disconnected")
